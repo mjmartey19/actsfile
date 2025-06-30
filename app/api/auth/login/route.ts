918f6f550b24482dd866fedf4709648e4f97a8af
Scholar <schorlar2468@gmail.com>
2025-06-30 19:24:32 +0000

    Changed user name

--- app/api/auth/login/route.ts
+++ app/api/auth/login/route.ts
@@ -6,7 +6,7 @@ const users = [
     id: "1",
     email: "scholar@gmail.com",
     password: "$2a$10$92IXUNpkjO0rOQ5byMi.Ye4oKoEa3Ro9llC/.og/at2.uheWG/igi", // password
-    name: "John Doe",
+    name: "Martey Jamel",
     phone: "+233123456789",
     avatar: "/placeholder-user.jpg",
     bio: "Passionate about sustainable development and community empowerment.",
